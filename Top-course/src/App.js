529792fc4f8f5ca7b585c78754341b3389676a2f
Ritikpanditonly <pandeyritik236@gmail.com>
2026-04-09 14:52:12 +0530

    complete all project

--- Top-course/src/App.js
+++ Top-course/src/App.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React from "react";
 import Navbar from "./components/Navbar";
-import Filter from "./components/Filter";
-import Cards from "./components/Cards";
-import { apiUrl, filterData } from './data';
-import { toast } from 'react-toastify';
-import { useState, useEffect } from 'react';
+import Cards from "./components/Cards"
+import Filter from "./components/Filter"
+import { apiUrl, filterData } from "./data";
+import { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
+import { toast } from "react-toastify";
+
 
 const App = () => {
-
   const [courses, setCourses] = useState(null);
   const [loading, setLoading] = useState(true);
   const [category, setCategory] = useState(filterData[0].title);
@@ -30,21 +31,31 @@ const App = () => {
     fetchData();
   }, [])
 
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col bg-bgDark2">
       <div>
         <Navbar />
       </div>
-
-      <div>
-        <Filter filterData={filterData} />
+      <div className="bg-bgDark2">
+        <div>
+          <Filter
+            filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+          />
+        </div>
+        <div className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+          {
+            loading ? (<Spinner />) : (<Cards courses={courses} category={category} />)
+          }
+        </div>
       </div>
 
-      <div>
-        loading ? (<spinner />) : (<Cards />);
-      </div>
+
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
